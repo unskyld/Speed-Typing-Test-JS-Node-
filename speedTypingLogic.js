db1f9844be3789f7eec874686da4eba91754d732
Arsenij <arseniy.ivanov421@gmail.com>
2024-06-18 18:54:08 +0200

    updated logic

--- speedTypingLogic.js
+++ speedTypingLogic.js
@@ -7,56 +7,82 @@ const rl = readline.createInterface({
 
 
 //mode selection
-console.log("This is a speed typing test thqat measures accuracy and speed of typing. Please select mode.");
-let modeOneMin, modeText;
+console.log('\x1b[36m%s\x1b[0m', "This is a speed typing test that measures accuracy and speed of typing. Please select mode.");
+let modeOneMin = false , modeText = false;
 function promptModeSelection(){
-rl.question('Which mode do you prefer? One minute mode or text writing mode? Enter: "One minute" or "Text" to select.', (mode) => {
+rl.question('\x1b[36m%s\x1b[0m', 'Which mode do you prefer? One minute mode or text writing mode? Enter: "One minute" or "Text" to select.', (mode) => {
     
     if(mode.toLowerCase() === "one minute"){ 
-    console.log('Selected One Minute Mode');
+    console.log('\x1b[36m%s\x1b[0m','Selected One Minute Mode');
     modeOneMin = true;
     modeText = false;
     rl.close();
+    startTesting();
 
 }   else if(mode.toLowerCase() === "text"){ 
-    console.log('Selected Text Writing mode.');
+    console.log('\x1b[36m%s\x1b[0m','Selected Text Writing mode.');
     modeText = true;
     modeOneMin = false;
     rl.close();
+    startTesting();
 }
     else if(modeOneMin == false || modeText == false){ 
-        console.log("Please select an existing mode");
+        console.log('\x1b[36m%s\x1b[0m',"Please select an existing mode");
         promptModeSelection();
     }
 
 })
 }
 promptModeSelection();
+
+//implementing time measurement
 let time = 60000;
-function counter(){ 
+let interval;
+function timeCount(){ 
     time -= 1000;
-    process.stdout.write(`Time remaining: ${time/1000} seconds. \r`);
+    process.stdout.write(`\x1b[36m\x1b[0m, \rTime remaining: ${time/1000} seconds. `);
     if(time=== 0){
-        clearInterval()
-        console.log("Time's up! Your result:")
+        clearInterval(interval);
+        console.log('\x1b[36m%s\x1b[0m',"Time's up! Your result:");
     }
 }
-function intervalForCounter(counter){
-    setInterval(counter,1000);
+function intervalForCounter(timeCount){
+    interval = setInterval(timeCount,1000);
 }
-if(modeOneMin == true){ 
-
-}
-
-if(modeText == true){ 
-}
-function modeTextFunction(){
-    const start = performance.now();
-    const end = performance.now();
-    
-    const elapsed = end - start;
-    console.log(`Task took ${elapsed} milliseconds`);
-}
+//implementing one-minute mode
+/*if(modeOneMin == true){ 
+ startTesting();
+}*/
 
 let wordlist= ["reptile","norm","silver", ]
-console.log("\x1b[38;5;196mHello\x1b[0m");
+//function to shuffle array
+function shuffle(array) {
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+  
+//function to display words, set off the timer 
+function startTesting(){
+    shuffle(wordlist);
+    //displaying the first word and waiting for user to start typing
+    process.stdout.write(wordlist[0]);
+    console.log('\x1b[36m%s\x1b[0m', ' The clock starts with the first key press. Good luck.');
+    for(i = 0; i<(wordlist.length); i++){
+        process.stdout.write(`\r${wordlist[i]} `);
+    
+    }
+    if(!interval){
+    rl.input.on('keypress', (str, key) => {
+        intervalForCounter()});
+}
+}
+
+
+//red console.log(`\x1b[38;5;196m${}\x1b[0m`);
+//green console.log(`\x1b[32m${}\x1b[0m`);
